96ace46581177edf788b8d11349eb812425f6628
Shiv Bhonde <shivbhonde04@gmail.com>
2022-02-06 21:54:49 +0530

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,7 +5,7 @@ const ContactUs = () => {
 	return (
 		<div className="max-w-4xl mx-auto flex flex-col items-center justify-center my-20">
 			<p className="text-center text-2xl md:text-4xl font-semibold text-black">
-				Intersed in working together ?
+				Interested in working together ?
 			</p>
 			<a
 				className="bg-black text-white p-3 rounded-xl mt-7 inline-block"
